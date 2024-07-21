5f72b73ef00f6af155ce5586d227b8a99d0b0efb
Jaqueline Silva <jscnewlf@gmail.com>
2024-07-21 12:00:21 -0300

    ajuste feed

--- packages/web/src/pages/Feed.tsx
+++ packages/web/src/pages/Feed.tsx
@@ -1,3 +1,4 @@
+// Feed.tsx
 import React, { useEffect, useState } from 'react';
 import axios from '../axiosConfig';
 import { useNavigate } from 'react-router-dom';
@@ -19,7 +20,7 @@ const Feed: React.FC = () => {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await axios.get('http://localhost:3001/api/post/user/posts', { withCredentials: true });
+                const response = await axios.get('/api/post/user/posts', { withCredentials: true });
                 setPosts(response.data);
                 setLoading(false);
             } catch (err) {
@@ -36,8 +37,7 @@ const Feed: React.FC = () => {
             await axios.post('/api/post/create', newPost, { withCredentials: true });
             setNewPost({ title: '', content: '' });
             setShowCreateForm(false);
-            // Refetch posts after creating a new post
-            const response = await axios.get('http://localhost:3001/api/post/user/posts', { withCredentials: true });
+            const response = await axios.get('/api/post/user/posts', { withCredentials: true });
             setPosts(response.data);
         } catch (err) {
             setError('Erro ao criar post');
@@ -46,8 +46,7 @@ const Feed: React.FC = () => {
 
     const handleDeletePost = async (id: number) => {
         try {
-            await axios.delete(`http://localhost:3001/api/post/remove/${id}`, { withCredentials: true });
-            // Remove post from state after deletion
+            await axios.delete(`/api/post/remove/${id}`, { withCredentials: true });
             setPosts(posts.filter(post => post.id !== id));
         } catch (err) {
             setError('Erro ao excluir post');
@@ -55,82 +54,86 @@ const Feed: React.FC = () => {
     };
 
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Feed de Posts</h1>
-            {loading && <p>Carregando...</p>}
-            {error && <p className="text-red-500">{error}</p>}
-            {!loading && posts.length === 0 && (
-                <div>
-                    <p>Nenhum post criado ainda.</p>
-                    <button
-                        className="mt-4 p-2 bg-blue-500 text-white rounded"
-                        onClick={() => setShowCreateForm(true)}
-                    >
-                        Criar Novo Post
-                    </button>
-                </div>
-            )}
-            {posts.length > 0 && (
-                <div>
-                    <button
-                        className="mb-4 p-2 bg-blue-500 text-white rounded"
-                        onClick={() => setShowCreateForm(true)}
-                    >
-                        Criar Novo Post
-                    </button>
-                    {posts.map(post => (
-                        <div key={post.id} className="mb-4 p-4 border border-gray-300 rounded">
-                            <h2 className="text-xl font-semibold">{post.title}</h2>
-                            <p>{post.content}</p>
-                            <div className="mt-2">
-                                <button
-                                    className="mr-2 p-2 bg-yellow-500 text-white rounded"
-                                    onClick={() => navigate(`/feed/post_${post.id}`)}
-                                >
-                                    Ver Detalhes
-                                </button>
-                                <button
-                                    className="mr-2 p-2 bg-red-500 text-white rounded"
-                                    onClick={() => handleDeletePost(post.id)}
-                                >
-                                    Excluir
-                                </button>
+        <div>
+            <div className='customized-bg before:h-6 before:top-16 before:shadow-md before:z-[2] small-animated'></div>
+            <div className=' flex items-center justify-center h-screen relative z-1 flex-col '>
+                {loading && <p>Carregando...</p>}
+                {error && <p className="text-red-500">{error}</p>}
+                {!loading && posts.length === 0 && (
+                    <div className='flex flex-col items-center'>
+                        <p className='text-darkblue-800'>Nenhum post criado ainda.</p>
+                        <button
+                            className="mt-4 p-2 bg-darkblue-600 text-white rounded"
+                            onClick={() => setShowCreateForm(true)}
+                        >
+                            Criar Novo Post
+                        </button>
+                    </div>
+                )}
+                {posts.length > 0 && (
+                    <div className='flex items-center justify-center h-screen relative z-1 flex-col w-full  sm:w-11/12 '>
+                        <button
+                            className="mb-6 p-2 bg-darkblue-600 text-white rounded"
+                            onClick={() => setShowCreateForm(true)}
+                        >
+                            Criar Novo Post
+                        </button>
+                        {posts.map(post => (
+                            <div key={post.id} className="mb-4 p-4 border-t-2 border-softblue-600 w-full max-w-md relative">
+                                <h2 className="text-xl font-semibold text-darkblue-600">{post.title}</h2>
+                                <p className='text-darkblue-400'>{post.content}</p>
+                                <div className=" absolute right-0 top-2">
+                                    <button
+                                        className="mr-2 p-1 bg-darkblue-200 text-white rounded hover:bg-opacity-80 text-xs"
+                                        onClick={() => navigate(`/feed/post/${post.id}`)}
+                                    >
+                                        Ver Detalhes
+                                    </button>
+                                    <button
+                                        className="p-1 bg-grayish-800 text-white rounded hover:bg-opacity-80 text-xs"
+                                        onClick={() => handleDeletePost(post.id)}
+                                    >
+                                        Excluir
+                                    </button>
+                                </div>
                             </div>
+                        ))}
+                    </div>
+                )}
+                {showCreateForm && (
+                    <div className='fixed inset-0 flex items-center justify-center bg-[#0000002e]'>
+                        <div className="mt-4 p-4 border border-gray-100 rounded shadow-2xl max-w-md bg-white">
+                            <h2 className="text-xl font-semibold text-darkblue-800 mb-4">Novo Post</h2>
+                            <input
+                                type="text"
+                                placeholder="Título"
+                                className="w-full p-2 border border-gray-300 rounded mb-2"
+                                value={newPost.title}
+                                onChange={e => setNewPost({ ...newPost, title: e.target.value })}
+                            />
+                            <textarea
+                                placeholder="Conteúdo"
+                                className="w-full p-2 border border-gray-300 rounded mb-2"
+                                rows={4}
+                                value={newPost.content}
+                                onChange={e => setNewPost({ ...newPost, content: e.target.value })}
+                            />
+                            <button
+                                className="mr-2 p-2 bg-darkblue-200 text-white rounded hover:bg-opacity-80"
+                                onClick={handleCreatePost}
+                            >
+                                Criar Post
+                            </button>
+                            <button
+                                className="p-2 bg-grayish-800 text-white rounded hover:bg-opacity-80"
+                                onClick={() => setShowCreateForm(false)}
+                            >
+                                Cancelar
+                            </button>
                         </div>
-                    ))}
-                </div>
-            )}
-            {showCreateForm && (
-                <div className="mt-4 p-4 border border-gray-300 rounded">
-                    <h2 className="text-xl font-semibold">Criar Novo Post</h2>
-                    <input
-                        type="text"
-                        placeholder="Título"
-                        className="w-full p-2 border border-gray-300 rounded mb-2"
-                        value={newPost.title}
-                        onChange={e => setNewPost({ ...newPost, title: e.target.value })}
-                    />
-                    <textarea
-                        placeholder="Conteúdo"
-                        className="w-full p-2 border border-gray-300 rounded mb-2"
-                        rows={4}
-                        value={newPost.content}
-                        onChange={e => setNewPost({ ...newPost, content: e.target.value })}
-                    />
-                    <button
-                        className="mr-2 p-2 bg-blue-500 text-white rounded"
-                        onClick={handleCreatePost}
-                    >
-                        Criar Post
-                    </button>
-                    <button
-                        className="p-2 bg-gray-500 text-white rounded"
-                        onClick={() => setShowCreateForm(false)}
-                    >
-                        Cancelar
-                    </button>
-                </div>
-            )}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
